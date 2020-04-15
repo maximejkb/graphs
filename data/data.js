f5665b9b182509bbe3b7b14098ba7e065c42a491
Maxime Kawawa-Beaudan <maxime.jkb@gmail.com>
2020-04-14 17:53:41 -0700

    ER Graph model.

--- data/data.js
+++ data/data.js
@@ -1,205 +1,129 @@
-//Dummy data just for development use. Array of JSON-style objects.
-var data = [
-  {label: "0"},
-  {label: "1"},
-  {label: "2"},
-  {label: "3"},
-  {label: "4"},
-  {label: "5"},
-  {label: "6"},
-  {label: "7"},
-  {label: "8"},
-  {label: "9"},
-  {label: "10"},
-  {label: "11"},
-  {label: "12"},
-  {label: "13"},
-  {label: "14"},
-  {label: "15"},
-  {label: "16"},
-  {label: "17"},
-  {label: "18"},
-  {label: "19"},
-  {label: "20"},
-  {label: "21"},
-  {label: "22"},
-  {label: "23"},
-  {label: "24"},
-  {label: "25"},
-  {label: "26"},
-  {label: "27"},
-  {label: "28"},
-  {label: "29"},
-  {label: "30"},
-  {label: "31"},
-  {label: "32"},
-  {label: "33"},
-  {label: "34"},
-  {label: "35"},
-  {label: "36"},
-  {label: "37"},
-  {label: "38"},
-  {label: "39"},
-  {label: "40"},
-  {label: "41"},
-  {label: "42"},
-  {label: "43"},
-  {label: "44"},
-  {label: "45"},
-  {label: "46"},
-  {label: "47"},
-  {label: "48"},
-  {label: "49"},
-  {label: "50"},
-  {label: "51"},
-  {label: "52"},
-  {label: "53"},
-  {label: "54"},
-  {label: "55"},
-  {label: "56"},
-  {label: "57"},
-  {label: "58"},
-  {label: "59"},
-  {label: "60"},
-  {label: "61"},
-  {label: "62"},
-  {label: "63"},
-  {label: "64"},
-  {label: "65"},
-  {label: "66"},
-  {label: "67"},
-  {label: "68"},
-  {label: "69"},
-  {label: "70"},
-  {label: "71"},
-  {label: "72"},
-  {label: "73"},
-  {label: "74"},
-  {label: "75"},
-  {label: "76"},
-  {label: "77"},
-  {label: "78"},
-  {label: "79"},
-  {label: "80"},
-];
-var edges = [
-  {source: "1", target: "2", distance: 30},
-  {source: "1", target: "3", distance: 30},
-  {source: "1", target: "4", distance: 20},
-  {source: "1", target: "5", distance: 20},
-  {source: "1", target: "6", distance: 10},
-  {source: "2", target: "31", distance: 50},
-  {source: "2", target: "16", distance: 50},
-  {source: "3", target: "2", distance: 30},
-  {source: "3", target: "9", distance: 60},
-  {source: "4", target: "25", distance: 30},
-  {source: "5", target: "27", distance: 30},
-  {source: "6", target: "30", distance: 60},
-  {source: "15", target: "2", distance: 60},
-  {source: "16", target: "1", distance: 50},
-  {source: "16", target: "32", distance: 50},
-  {source: "16", target: "39", distance: 40},
-  {source: "16", target: "38", distance: 40},
-  {source: "18", target: "2", distance: 20},
-  {source: "21", target: "39", distance: 20},
-  {source: "31", target: "38", distance: 30},
-  {source: "31", target: "1", distance: 30},
-  {source: "31", target: "10", distance: 30},
-  {source: "31", target: "3", distance: 40},
-  {source: "31", target: "4", distance: 30},
-  {source: "31", target: "5", distance: 30},
-  {source: "31", target: "6", distance: 30},
-  {source: "31", target: "7", distance: 30},
-  {source: "31", target: "8", distance: 30},
-  {source: "31", target: "9", distance: 30},
-  {source: "31", target: "11", distance: 30},
-  {source: "14", target: "12", distance: 30},
-  {source: "14", target: "13", distance: 30},
-  {source: "14", target: "14", distance: 30},
-  {source: "14", target: "15", distance: 30},
-  {source: "14", target: "16", distance: 50},
-  {source: "8", target: "17", distance: 40},
-  {source: "8", target: "18", distance: 40},
-  {source: "8", target: "19", distance: 50},
-  {source: "8", target: "20", distance: 40},
-  {source: "8", target: "21", distance: 30},
-  {source: "8", target: "22", distance: 40},
-  {source: "36", target: "23", distance: 50},
-  {source: "36", target: "24", distance: 30},
-  {source: "36", target: "25", distance: 30},
-  {source: "36", target: "26", distance: 30},
-  {source: "36", target: "27", distance: 30},
-  {source: "36", target: "28", distance: 30},
-  {source: "23", target: "29", distance: 20},
-  {source: "23", target: "30", distance: 20},
-  {source: "23", target: "32", distance: 20},
-  {source: "23", target: "33", distance: 20},
-  {source: "23", target: "34", distance: 40},
-  {source: "23", target: "35", distance: 40},
-  {source: "34", target: "36", distance: 60},
-  {source: "34", target: "37", distance: 60},
-  {source: "34", target: "38", distance: 40},
-  {source: "34", target: "39", distance: 30},
-  {source: "40", target: "41", distance: 30},
-  {source: "41", target: "42", distance: 30},
-  {source: "42", target: "43", distance: 30},
-  {source: "43", target: "44", distance: 30},
-  {source: "45", target: "46", distance: 30},
-  {source: "43", target: "46", distance: 30},
-  {source: "46", target: "47", distance: 30},
-  {source: "69", target: "50", distance: 10},
-  {source: "69", target: "51", distance: 10},
-  {source: "69", target: "49", distance: 10},
-  {source: "49", target: "50", distance: 15},
-  {source: "49", target: "48", distance: 30},
-  {source: "50", target: "51", distance: 15},
-  {source: "50", target: "55", distance: 30},
-  {source: "51", target: "49", distance: 15},
-  {source: "51", target: "56", distance: 30},
-  {source: "49", target: "52", distance: 30},
-  {source: "50", target: "53", distance: 30},
-  {source: "51", target: "54", distance: 30},
-  {source: "48", target: "52", distance: 30},
-  {source: "54", target: "56", distance: 30},
-  {source: "55", target: "53", distance: 30},
-  {source: "48", target: "56", distance: 30},
-  {source: "52", target: "55", distance: 30},
-  {source: "53", target: "54", distance: 30},
-  {source: "48", target: "57", distance: 30},
-  {source: "48", target: "58", distance: 30},
-  {source: "57", target: "58", distance: 30},
-  {source: "58", target: "59", distance: 30},
-  {source: "52", target: "59", distance: 30},
-  {source: "52", target: "60", distance: 30},
-  {source: "59", target: "60", distance: 30},
-  {source: "60", target: "61", distance: 30},
-  {source: "55", target: "61", distance: 30},
-  {source: "55", target: "62", distance: 30},
-  {source: "61", target: "62", distance: 30},
-  {source: "62", target: "63", distance: 30},
-  {source: "53", target: "63", distance: 30},
-  {source: "53", target: "64", distance: 30},
-  {source: "63", target: "64", distance: 30},
-  {source: "64", target: "65", distance: 30},
-  {source: "54", target: "65", distance: 30},
-  {source: "54", target: "66", distance: 30},
-  {source: "65", target: "66", distance: 30},
-  {source: "66", target: "67", distance: 30},
-  {source: "56", target: "67", distance: 30},
-  {source: "56", target: "68", distance: 30},
-  {source: "67", target: "68", distance: 30},
-  {source: "57", target: "68", distance: 30},
-  {source: "70", target: "71", distance: 20},
-  {source: "70", target: "72", distance: 10},
-  {source: "71", target: "72", distance: 50},
-  {source: "71", target: "73", distance: 110},
-  {source: "71", target: "74", distance: 30},
-  {source: "74", target: "72", distance: 10},
-  {source: "72", target: "75", distance: 150},
-  {source: "74", target: "72", distance: 10},
-  {source: "74", target: "75", distance: 40},
-  {source: "73", target: "74", distance: 20},
-  {source: "74", target: "76", distance: 50},
-  {source: "75", target: "76", distance: 10},
-  {source: "76", target: "73", distance: 10},
-]
+var N = 200;
+var p = Math.log(N) / N - 0.015;
+var maxLength = 60;
+var minLength = 10;
+
+var data = [];
+var edges = [];
+for (var i = 0; i < N; i++) {
+  data.push({label: i.toString()})
+
+  for (var j = i; j < N; j++) {
+    if (Math.random() < p) {
+      edges.push({
+        source: i.toString(),
+        target: j.toString(),
+        distance: Math.floor((Math.random() * (maxLength - minLength + 1))) + minLength
+      })
+    }
+  }
+}
+
+function addNode() {
+  //What's the largest label we have? Our new node should have this value + 1 as its label.
+  var maxLabel = Math.max.apply(Math, data.map(node => parseInt(node.label)));
+  //@source: How to get the mouse x and mouse y position within the SVG canvas, so it's not
+  //weirdly translated: https://stackoverflow.com/questions/16770763/mouse-position-in-d3
+  var mouseX = d3.mouse(this)[0];
+  var mouseY = d3.mouse(this)[1];
+  //Initialize the new node to spawn under the mouse.
+  var newNode = {
+     label : (maxLabel + 1).toString(),
+     x : mouseX,
+     y : mouseY
+    };
+  //Add the new node to our data.
+  data.push(newNode);
+
+  //We select all the nodes. The second argument of data is a key function: how do we
+  //bind the new data to elements -- we bind data points to circles with matching IDs.
+  //This is using D3's "general update pattern". We "join" new data with existing elements.
+  nodeElements = nodeGroup.selectAll('circle')
+    .data(data, node => node.id);
+  //We remove all of the old (unchanged) data from the graph, so that we don't get duplicates
+  //of every existing node. D3 handles removing old data and not removing new data.
+  nodeElements.exit().remove();
+
+  //We enter the nodeElements object, add a circle for every piece of data, just as before.
+  var nodeEnter = nodeElements.enter()
+    .append('circle')
+    .attr('r', 10)
+    .attr('fill', 'LightGray')
+    .call(dragDrop);
+
+  //We merge the new nodeElements (data object) and the circles we've created.
+  nodeElements = nodeEnter.merge(nodeElements);
+
+  //We assign nodeElements to call startAlgorithm when they're clicked.
+  nodeElements.on("click", startAlgorithm);
+  /**
+  edgeElements = edgeGroup.selectAll("line")
+    //To make sure that we're uniquely binding the lines to the edge data, we use a
+    //unique identifier to match existing lines to old data -- the source label plus
+    //the target label. There is at most one edge connecting two nodes. But a node may
+    //have degree greater than one, so to distinguish between incident edges to the same
+    //source or target we identify an edge by the sum of the labels.
+    .data(edges, edge => edge.source.label + edge.target.label);
+
+  edgeElements.exit().remove();
+
+  var edgeEnter = edgeElements.enter()
+    .append("line")
+    .attr("stroke-width", 1)
+    .attr("stroke", "LightGray");
+
+  edgeElements = edgeEnter.merge(edgeElements);
+  **/
+  //We run the same exact procedure on the node labels.
+  textElements = labelGroup.selectAll('text')
+    .data(data, node => node.label);
+
+  textElements.exit().remove();
+
+  var textEnter = textElements.enter()
+    .append('text')
+    .text(node => node.label)
+    .attr("font-size", 15)
+    .attr("fill", "DarkGray")
+    .attr("fill-opacity", 1)
+    .attr("dx", 15)
+    .attr("dy", 5);
+
+  textElements = textEnter.merge(textElements);
+
+  //This is the exact same "tick" function as from app.js. We just have to describe again
+  //what we want to simulation to do since we're updating the simulation.
+  simulation.nodes(data).on('tick', () => {
+    //Increment timer and runDFS if the search is still ongoing.
+    if (!searchComplete) {
+      //If the simulation's alpha ("cooling parameter") falls below the threshold
+      //simulation.alphaMin (generally 0.001), ticking stops. Can't have this
+      //happening mid-search, so keep alpha artificially elevated.
+      simulation.alphaTarget(0.1).restart();
+      if (timer == 0) {
+        runAlgorithm();
+      }
+    }
+
+    //(11 - x) in order to reverse the value so that left is slow, right is quick,
+    //on the range [0, 10].
+    var interval = 21 - document.getElementById("animationspeed").value;
+    timer = (timer + 1) % interval;
+
+    nodeElements
+      .attr("cx", node => node.x)
+      .attr("cy", node => node.y);
+    textElements
+      .attr("x", node => node.x)
+      .attr("y", node => node.y);
+    edgeElements
+      .attr("x1", edge => edge.source.x)
+      .attr("x2", edge => edge.target.x)
+      .attr("y1", edge => edge.source.y)
+      .attr("y2", edge => edge.target.y);
+    weightElements
+      .attr("x", edge => (edge.source.x + edge.target.x) / 2)
+      .attr("y", edge => (edge.source.y + edge.target.y) / 2);
+  });
+
+}
